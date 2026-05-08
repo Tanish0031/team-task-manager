2daae889a391862ba6f258cf510efe6236fb5038
Tanish0031 <tanish@email.com>
2026-05-09 03:16:47 +0530

    fix: wildcards

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
-app.options('*', cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
